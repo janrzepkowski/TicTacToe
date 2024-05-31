78b37d570ce9156c879bfdee3a7c4fe3ca38a0e6
Jan Rzepkowski 247777 <jan.rzepkowski21@gmail.com>
2024-05-31 22:00:43 +0200

    Stream.io API keys

--- server/src/index.jsx
+++ server/src/index.jsx
@@ -1,7 +1,11 @@
 import express from "express";
 import cors from "cors";
+import { StreamChat } from "stream-chat";
 
 const app = express();
+const api_key = process.env.REACT_APP_API_KEY;
+const api_secret = process.env.REACT_APP_API_SECRET;
+const serverClient = new StreamChat.getInstance(api_key, api_secret);
 
 app.use(cors());
 app.use(express.json());
@@ -9,4 +13,3 @@ app.use(express.json());
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
 });
-
