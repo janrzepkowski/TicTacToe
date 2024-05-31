de49def352aa66bb57f859fe3fac65933f2bbec7
janrzepkowski <jan.rzepkowski21@gmail.com>
2024-05-31 23:21:46 +0200

    User ID, hashed password & sign up backend

--- server/src/index.jsx
+++ server/src/index.jsx
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import { StreamChat } from "stream-chat";
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
 
 const app = express();
 const api_key = process.env.REACT_APP_API_KEY;
@@ -9,6 +11,27 @@ const serverClient = new StreamChat.getInstance(api_key, api_secret);
 
 app.use(cors());
 app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  try {
+    const { firstName, lastName, username, password } = req.body;
+    const userID = uuidv4();
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const token = serverClient.createToken(userID);
+    res.json({ token, firstName, lastName, username, hashedPassword });
+  } catch (error) {
+    console.error(error);
+    res.json({ error: "Something went wrong" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const token = serverClient.createToken(username);
+  res.status(200).json({ token, username });
+});
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
